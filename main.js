646c81d038f423636c03c8d859e44517afe70ba0
CinnamonTeaa <shervinelibox569@gmail.com>
2023-12-01 20:57:05 -0400

    fixed my bugs

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function convert() {
     var num = parseFloat(document.querySelector("#num1").value);
     var selected = document.querySelector("#currency").value;
     var calculate;
-    const yen1 = 54.32; Euro1 = 0.34; USD1 = 0.37; Pounds1 = 0.29; Canada1 = 0.50; Korean1 = 479.12;
+    const yen1 = 54.32, Euro1 = 0.34, USD1 = 0.37, Pounds1 = 0.29, Canada1 = 0.50, Korean1 = 479.12;
 
     if (selected == "jap") {
 
@@ -19,24 +19,27 @@ function convert() {
         document.querySelector("#results").innerHTML = "€" + calculate.toFixed(2);
 
 
-    } if (selected == "USD") {
+    } else if (selected == "USD") {
 
         calculate = num * USD1;
         document.querySelector("#results").innerHTML = "$" + calculate.toFixed(2);
 
 
-    } if (selected == "Pounds") {
+    } else if (selected == "Pounds") {
 
         calculate = num * Pounds1;
         document.querySelector("#results").innerHTML = "£" + calculate.toFixed(2);
 
 
-    } if (selected == "Korean") {
+    } else if (selected == "Canada") {
+
+        calculate = num * Canada1;
+        document.querySelector("#results").innerHTML = "$" + calculate.toFixed(2);
+
+    } else if (selected == "Korean") {
 
         calculate = num * Korean1;
         document.querySelector("#results").innerHTML = "₩" + calculate.toFixed(2);
-
-
     }
 
 }

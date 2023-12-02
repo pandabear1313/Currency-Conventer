545ebcae562dea5b8d8af25f67cd5662c1c0fc73
CinnamonTeaa <shervinelibox569@gmail.com>
2023-12-01 20:35:46 -0400

    adding a few currency

--- main.js
+++ main.js
@@ -2,10 +2,41 @@
 
 function convert() {
 
-    var num1 = parseFloat(document.querySelector("#value").value);
+    var num = parseFloat(document.querySelector("#num1").value);
     var selected = document.querySelector("#currency").value;
     var calculate;
-    
+    const yen = 54.32; USD = 2.70; Euro = ;
 
+    if (selected == "jap") {
+
+        calculate = num * yen;
+        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+
+
+    } else if (selected == "Euro") {
+
+        calculate = num / 2.7;
+        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+
+
+    } if (selected == "USD") {
+
+        calculate = num * yen;
+        document.querySelector("#results").innerHTML = "$" + calculate.toFixed(2);
+
+
+    } if (selected == "Pounds") {
+
+        calculate = num * yen;
+        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+
+
+    } if (selected == "Korean") {
+
+        calculate = num * yen;
+        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+
+
+    }
 
 }

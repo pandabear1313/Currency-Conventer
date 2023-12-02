cee3f29c38d4608383001c10cbbfd49ea1abc545
CinnamonTeaa <shervinelibox569@gmail.com>
2023-12-01 20:47:51 -0400

    fixed a few errors in the code

--- main.js
+++ main.js
@@ -5,36 +5,36 @@ function convert() {
     var num = parseFloat(document.querySelector("#num1").value);
     var selected = document.querySelector("#currency").value;
     var calculate;
-    const yen = 54.32; USD = 2.70; Euro = ;
+    const yen1 = 54.32; Euro1 = 0.34; USD1 = 0.37; Pounds1 = 0.29; Canada1 = 0.50; Korean1 = 479.12;
 
     if (selected == "jap") {
 
-        calculate = num * yen;
+        calculate = num * yen1;
         document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
 
 
     } else if (selected == "Euro") {
 
-        calculate = num / 2.7;
-        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+        calculate = num * Euro1;
+        document.querySelector("#results").innerHTML = "€" + calculate.toFixed(2);
 
 
     } if (selected == "USD") {
 
-        calculate = num * yen;
+        calculate = num * USD1;
         document.querySelector("#results").innerHTML = "$" + calculate.toFixed(2);
 
 
     } if (selected == "Pounds") {
 
-        calculate = num * yen;
-        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+        calculate = num * Pounds1;
+        document.querySelector("#results").innerHTML = "£" + calculate.toFixed(2);
 
 
     } if (selected == "Korean") {
 
-        calculate = num * yen;
-        document.querySelector("#results").innerHTML = "¥" + calculate.toFixed(2);
+        calculate = num * Korean1;
+        document.querySelector("#results").innerHTML = "₩" + calculate.toFixed(2);
 
 
     }
